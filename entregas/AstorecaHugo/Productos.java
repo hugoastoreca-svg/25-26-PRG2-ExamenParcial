4d5dfcdf0950db3b0f017626e90005b3f28b4ff1
hugoastoreca <hugo.astoreca@alumnos.uneatlantico.es>
2026-04-01 12:55:32 +0200

    Creacion de los atributos para la implementacion de la clase Productos

---
+++ entregas/AstorecaHugo/Productos.java
@@ -0,0 +1,34 @@
+
+class Productos {
+private String nombreproducto;
+private String codigoProducto;
+private int precioProducto;
+private int stockProducto;
+    public Productos(String nombreProducto, String codigoProducto, int precioProducto, int stockProducto) {
+    }
+
+    public String nombrarProducto() {
+
+    }
+
+    public String nombrarCodigo() {
+
+    }
+
+    public int ponerPrecio() {
+
+    }
+
+    public int ponerStock() {
+
+    }
+
+    public int preguntarPrecio(String nombreProducto) {
+
+    }
+
+    public int preguntarStock(String nombreProducto, int stockProducto) {
+
+    }
+
+}
