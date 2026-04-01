33c18d215c1066f70d4c23a792ddc82f053add8c
hugoastoreca <hugo.astoreca@alumnos.uneatlantico.es>
2026-04-01 12:41:37 +0200

    Definición de atributos privados para la clase Tienda e implementacion a la vista publica

---
+++ entregas/AstorecaHugo/Tienda.java
@@ -0,0 +1,14 @@
+class Tienda{
+    private String nombreTienda;
+    
+    public Tienda( String nombreTienda){
+        this.nombreTienda = nombreTienda;
+    }
+        public String nombrarTienda(){
+            return this.nombreTienda;
+        }
+        public void mostrar(){
+            
+        }
+    
+}
