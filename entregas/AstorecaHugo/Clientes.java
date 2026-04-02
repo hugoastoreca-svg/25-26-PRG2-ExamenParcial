d82f1c38b14cef97dda9233bcbaf38125fabeb48
hugoastoreca <hugo.astoreca@alumnos.uneatlantico.es>
2026-04-02 13:03:12 +0200

    Creacion de la clase clientes, y ademas creacion de los atributos para empezar la implementación

---
+++ entregas/AstorecaHugo/Clientes.java
@@ -0,0 +1,21 @@
+class Clientes {
+    private String nombreClientes;
+    private String correoClientes;
+    private String lugarClientes;
+    
+    public Clientes (String nombreClientes, String correoClientes, String lugarClientes){
+        
+    }
+    public String nombrarClientes(){
+
+    }
+    public String direccionarClientes(){
+
+    }
+    public String localizarClientes(){
+
+    }
+    public void lugarClientes(String nombreClientes){
+
+    }
+}
