ca99439d42fdd69ae6dd9eb8cb4923dad462ca25
hugoastoreca <hugo.astoreca@alumnos.uneatlantico.es>
2026-04-02 13:24:11 +0200

    creacion de la vista publkca de la clase Pedidos

---
+++ entregas/AstorecaHugo/Pedidos.java
@@ -0,0 +1,23 @@
+class Pedidos {
+    public Pedidos(String nombreComprador, String productosComprados, String fechaCompra,String estadoCompra){
+
+    }
+    public String nombrarComprador(){
+
+    }
+    public String seleccionarProductos(){
+
+    }
+    public String calendarizarFecha(){
+
+    }
+    public String consultarEstado(){
+
+    }
+    public void preguntarEstdado(String estadoCompra){
+
+    }
+    public void realizarPedido(String productosComprados){
+
+    }
+}
