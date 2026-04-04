3ffd36c80bd2671620bfe0fe529d5cde985c9c77
hugoastoreca <hugo.astoreca@alumnos.uneatlantico.es>
2026-04-04 12:13:19 +0200

    defino la estructura privada de la clase pedidos

--- entregas/AstorecaHugo/Pedidos.java
+++ entregas/AstorecaHugo/Pedidos.java
@@ -1,23 +1,36 @@
+
 class Pedidos {
-    public Pedidos(String nombreComprador, String productosComprados, String fechaCompra,String estadoCompra){
+
+    private String nombreComprador;
+    private String productosComprados;
+    private String fechaCompra;
+    private String estadoCompra;
+
+    public Pedidos(String nombreComprador, String productosComprados, String fechaCompra, String estadoCompra) {
 
     }
-    public String nombrarComprador(){
+
+    public String nombrarComprador() {
 
     }
-    public String seleccionarProductos(){
+
+    public String seleccionarProductos() {
 
     }
-    public String calendarizarFecha(){
+
+    public String calendarizarFecha() {
 
     }
-    public String consultarEstado(){
+
+    public String consultarEstado() {
 
     }
-    public void preguntarEstdado(String estadoCompra){
+
+    public void preguntarEstdado(String estadoCompra) {
 
     }
-    public void realizarPedido(String productosComprados){
+
+    public void realizarPedido(String productosComprados) {
 
     }
 }
