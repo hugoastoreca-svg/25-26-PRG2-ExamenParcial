113c634916400af4428622adf44645079262f10e
hugoastoreca <hugo.astoreca@alumnos.uneatlantico.es>
2026-04-05 12:57:01 +0200

    Creacionde metodo privado para hacer vivir al publico en l clase Clientes

--- entregas/AstorecaHugo/Clientes.java
+++ entregas/AstorecaHugo/Clientes.java
@@ -1,23 +1,33 @@
+
 class Clientes {
+
     private String nombreClientes;
     private String correoClientes;
     private String lugarClientes;
-    
-    public Clientes (String nombreClientes, String correoClientes, String lugarClientes){
+
+    public Clientes(String nombreClientes, String correoClientes, String lugarClientes) {
         this.nombreClientes = nombreClientes;
         this.correoClientes = correoClientes;
         this.lugarClientes = lugarClientes;
     }
-    public String nombrarClientes(){
+
+    public String nombrarClientes() {
         return this.nombreClientes;
     }
-    public String direccionarClientes(){
+
+    public String direccionarClientes() {
 
     }
-    public String localizarClientes(){
 
+    public String localizarClientes() {
+        return this.lugarClientes;
     }
-    public void lugarClientes(String nombreClientes){
+
+    private String obtenerLugar() {
+        return this.lugarClientes;
+    }
+
+    public void lugarClientes(String nombreClientes) {
 
     }
 }
